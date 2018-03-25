8a17f78f250e7349381e212a474da3ec57f7f856
ChiharuTerashima <chichi.pa2.1091@gmail.com>
2018-03-25 23:29:44 +0900

    トレンドチャート表示

--- dashboards/views/detail/views.py
+++ dashboards/views/detail/views.py
@@ -1,3 +1,4 @@
+import datetime
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from dashboards.models.trends import Trends
@@ -6,11 +7,13 @@ from dashboards.models.trends import Trends
 class TrendsDetailView(APIView):
 
     def get(self, request, day, format=None):
-        trends = Trends.objects.get(target_date=day)
+        d = datetime.datetime.strptime(day, '%Y%m%d')
+        trends = Trends.objects.filter(target_date=d.strftime('%Y-%m-%d'))
 
         lists = [{
-            "name": trend.word
+            "name": str(trend.count) + ":" + trend.word
             , "val": trend.count
         } for trend in trends]
+        response = {"children": lists}
 
-        return Response(lists)
+        return Response(response)
--- static/js/detail.js
+++ static/js/detail.js
@@ -1,53 +1,52 @@
-var data_set = {
-    children: [
-        {name:"aaa", val:58},
-        {name:"bbb", val:88},
-        {name:"ccc", val:48},
-        {name:"ddd", val:73},
-        {name:"eee", val:81},
-        {name:"fff", val:31}  ] } ;
-
-var width=600, height=300;
-var bubble = d3.pack()
-    .size([width, height])
-    .padding(1.5) ;
-
-var nodes = d3.hierarchy( data_set )
-    .sum(function(d){ return d.val })
-;
-
-var bubble_data = bubble(nodes).descendants() ;
-
-var no_root_bubble = bubble_data.filter( function(d){ return d.parent != null ;} ) ;
-
-var max_val = d3.max(no_root_bubble, function(d){ return d.r ;});
-var min_val = d3.min(no_root_bubble, function(d){return d.r ; });
-
-var color_scale = d3.scaleLinear()
-    .domain( [min_val, max_val] )
-    .range(d3.schemeCategory10 );
-var font_scale = d3.scaleLinear()
-    .domain([min_val, max_val])
-    .range([9, 28]);
-
-var bubbles = d3.select("#bubble_chart")
-    .selectAll(".bubble")
-    .data(no_root_bubble)
-    .enter()
-    .append("g")
-    .attr("class", "bubble")
-    .attr("transform", function(d){ return "translate("+d.x+","+d.y+")" ;})
-;
-
-bubbles.append("circle")
-    .attr("r", function(d){ return d.r })
-    .style("fill", function(d,i){
-        return color_scale(d.r);
+$(function(){
+    $.ajax({
+        url:'/dashboards/api/trends/' + day,
+        type:'GET',
+        data:{}
+    }).done((data) => {
+        // Ajaxリクエストが成功した時発動
+        bubbleChart(data);
+    }).fail((data) => {
+        // Ajaxリクエストが失敗した時発動
+        console.log(data);
+    }).always((data) => {
+        // Ajaxリクエストが成功・失敗どちらでも発動
     });
-;
-bubbles.append("text")
-    .attr("text-anchor", "middle")
-    .attr("dominant-baseline", "central")
-    .text(function(d){ return d.data.name ; })
-    .style("font-size", function(d){ return font_scale(d.r) ; })
-;
+});
+
+function bubbleChart(data_set) {
+    var width=1200, height=600;
+    var bubble = d3.pack().size([width, height]).padding(1.5);
+
+    var nodes = d3.hierarchy( data_set ).sum(function(d){ return d.val });
+
+    var bubble_data = bubble(nodes).descendants();
+
+    var no_root_bubble = bubble_data.filter( function(d){ return d.parent != null ;} );
+
+    var max_val = d3.max(no_root_bubble, function(d){ return d.r ;});
+    var min_val = d3.min(no_root_bubble, function(d){return d.r ; });
+
+    var color_scale = d3.scaleLinear().domain( [min_val, max_val] ).range(d3.schemeCategory10 );
+    var font_scale = d3.scaleLinear().domain([min_val, max_val]).range([9, 28]);
+
+    var bubbles = d3.select("#bubble_chart")
+        .selectAll(".bubble")
+        .data(no_root_bubble)
+        .enter()
+        .append("g")
+        .attr("class", "bubble")
+        .attr("transform", function(d){ return "translate("+d.x+","+d.y+")" ;});
+
+    bubbles.append("circle")
+        .attr("r", function(d){ return d.r })
+        .style("fill", function(d,i){
+            return color_scale(d.r);
+        });
+
+    bubbles.append("text")
+        .attr("text-anchor", "middle")
+        .attr("dominant-baseline", "central")
+        .text(function(d){ return d.data.name ; })
+        .style("font-size", function(d){ return font_scale(d.r) ; });
+}
